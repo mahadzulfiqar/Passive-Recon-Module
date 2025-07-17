42407cfb7100b8524414c4221b0ab6e3ffc50b5b
Mahad Zulfiqar <mahadzulfiqar2@gmail.com>
2025-07-17 16:43:01 +0500

    Deleted duplicate file after moving to scripts/ folder

--- whois_lookup.py
+++
@@ -1,9 +0,0 @@
-import whois
-
-domain = input("Enter domain for WHOIS lookup: ")
-try:
-    w = whois.whois(domain)
-    print(w)
-except Exception as e:
-    print(f"Error during WHOIS lookup: {e}")
-
